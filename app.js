12795b317d3ac5f870a68c94c66c362dc43f9812
Logan <logancable@ymail.com>
2023-02-27 13:06:23 -0800

    first commit

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 const express = require("express");
 const app = express();
+const morgan = require('morgan')
+const postBank = require('./postBank')
 
-app.get("/", (req, res) => res.send("Hello World!"));
+
+app.use(morgan('dev'));
+
+app.get("/", (req, res) => {
+  const posts = postBank.list();
+
+  const html = `<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+  </head>
+  <body>
+    <ul>
+      ${posts.map(post => `<li>${post.title}</li>`)}
+    </ul>
+  </body>
+  </html>`
+})
 
 const PORT = 1337;
 
