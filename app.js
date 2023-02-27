8cd4ac22bf5ea975b488dbaf20cf25b59f36a68a
MonkComputer <bmonk1996@yahoo.com>
2023-02-27 13:59:41 -0600

    formatting

--- app.js
+++ app.js
@@ -1,32 +1,24 @@
 const express = require("express");
 const app = express();
-const morgan = require('morgan')
-const postBank = require('./postBank')
-const timeAgo = require('node-time-ago');
+const morgan = require("morgan");
+const postBank = require("./postBank");
+const timeAgo = require("node-time-ago");
 const postDetails = require("./views/postDetails");
 
-
-app.use(morgan('dev'));
-app.use(express.static('public'))
-
+app.use(morgan("dev"));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   const posts = postBank.list();
- 
-  res.send(postLists(posts))
-})
+  res.send(postLists(posts));
+});
 
-app.get("/posts/:id",(req,res)=>{
+app.get("/posts/:id", (req, res) => {
   const posts = postBank.find(req.params.id);
- 
-    res.send(postDetails(posts))
-  }
-  
+  res.send(postDetails(posts));
+});
 
-})
-
-
-const {PORT = 1337} = process.env;
+const { PORT = 1337 } = process.env;
 
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
--- views/postDetails.js
+++ views/postDetails.js
@@ -1,48 +1,34 @@
-function postDetails(post) {
-    
-    try{
-      if (!post.id) {
-        // If the post wasn't found, just throw an error
-        throw new Error('Not Found')
-      }
-      index = req.params.id
-      const post = postBank.find(index)
-      try{
-        if (!post.id) {
-          // If the post wasn't found, just throw an error
-          throw new Error('Not Found')
-        }
-        const html = `<!DOCTYPE html>
-        <html>
-        <head>
-          <title>Wizard News</title>
-          <link rel="stylesheet" href="/style.css" />
-        </head>
-        <body>
-          <div class="news-list">
-            <header><img src="/logo.png"/>Wizard News</header>
-              <div class='news-item'>
-                <p>
-                  <span class="news-position">${post.id}. ▲</span>
-                  ${post.title}
-                  <small>(by ${post.name})</small>
-                </p>
-                <small class="news-info">
-                  ${post.upvotes} upvotes | ${post.date}
-                </small>
-                ${post.content}
-              </div>
-          </div>
-        </body>
-      </html>`
-        
-        
-        
-      
-        res.send(html)
-      }catch(error){
-        res.status(404)
-        const html = `
+try {
+  if (!post.id) {
+    // If the post wasn't found, just throw an error
+    throw new Error("Not Found");
+  }
+  const html = `<!DOCTYPE html>
+                <html>
+                <head>
+                  <title>Wizard News</title>
+                  <link rel="stylesheet" href="/style.css" />
+                </head>
+                <body>
+                  <div class="news-list">
+                    <header><img src="/logo.png"/>Wizard News</header>
+                      <div class='news-item'>
+                        <p>
+                          <span class="news-position">${post.id}. ▲</span>
+                          ${post.title}
+                          <small>(by ${post.name})</small>
+                        </p>
+                        <small class="news-info">
+                          ${post.upvotes} upvotes | ${post.date}
+                        </small>
+                        ${post.content}
+                      </div>
+                  </div>
+                </body>
+              </html>`;
+} catch (error) {
+  res.status(404);
+  const html = `
         <!DOCTYPE html>
         <html>
         <head>
@@ -55,7 +41,6 @@ function postDetails(post) {
             <p>404: Page Not Found</p>
           </div>
         </body>
-        </html>`
-      }
-}} 
-  module.exports = { postDetails: postDetails };
+        </html>`;
+}
+module.exports = { postDetails: postDetails };
