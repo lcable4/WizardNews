07e88c3e2b714868633ae6a0b62eab90779a101b
MonkComputer <bmonk1996@yahoo.com>
2023-02-27 14:21:34 -0600

    moved html into view folder

--- app.js
+++ app.js
@@ -3,19 +3,24 @@ const app = express();
 const morgan = require("morgan");
 const postBank = require("./postBank");
 const timeAgo = require("node-time-ago");
-const postDetails = require("./views/postDetails");
+const { postList } = require("./views/postList");
+const {postDetails } = require('./views/postDetails')
 
 app.use(morgan("dev"));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   const posts = postBank.list();
-  res.send(postLists(posts));
+  console.log(postList)
+  res.send(postList(posts, timeAgo));
 });
 
+
 app.get("/posts/:id", (req, res) => {
-  const posts = postBank.find(req.params.id);
-  res.send(postDetails(posts));
+  const post = postBank.find(req.params.id);
+  
+    res.send(postDetails(post,timeAgo));
+  
 });
 
 const { PORT = 1337 } = process.env;
--- views/postDetails.js
+++ views/postDetails.js
@@ -1,46 +1,53 @@
-try {
-  if (!post.id) {
-    // If the post wasn't found, just throw an error
-    throw new Error("Not Found");
-  }
-  const html = `<!DOCTYPE html>
-                <html>
-                <head>
-                  <title>Wizard News</title>
-                  <link rel="stylesheet" href="/style.css" />
-                </head>
-                <body>
-                  <div class="news-list">
-                    <header><img src="/logo.png"/>Wizard News</header>
-                      <div class='news-item'>
-                        <p>
-                          <span class="news-position">${post.id}. ▲</span>
-                          ${post.title}
-                          <small>(by ${post.name})</small>
-                        </p>
-                        <small class="news-info">
-                          ${post.upvotes} upvotes | ${post.date}
-                        </small>
-                        ${post.content}
+const postDetails = (post, timeAgo) => {
+  try {
+    if (!post.id) {
+      // If the post wasn't found, just throw an error
+      throw new Error("Not Found");
+    }
+    const html = `<!DOCTYPE html>
+                    <html>
+                    <head>
+                      <title>Wizard News</title>
+                      <link rel="stylesheet" href="/style.css" />
+                    </head>
+                    <body>
+                      <div class="news-list">
+        
+                      <header><img src="/logo.png"/>Wizard News</header>
+                          <div class='news-item'>
+                            <p>
+                              <span class="news-position">${post.id}. ▲</span>
+                              ${post.title}
+                              <small>(by ${post.name})</small>
+                            </p>
+                            <small class="news-info">
+                              ${post.upvotes} upvotes | ${timeAgo(
+      post.date.getTime()
+    )}
+                            </small>
+                            ${post.content}
+                          </div>
                       </div>
-                  </div>
-                </body>
-              </html>`;
-} catch (error) {
-  res.status(404);
-  const html = `
-        <!DOCTYPE html>
-        <html>
-        <head>
-          <title>Wizard News</title>
-          <link rel="stylesheet" href="/style.css" />
-        </head>
-        <body>
-          <header><img src="/logo.png"/>Wizard News</header>
-          <div class="not-found">
-            <p>404: Page Not Found</p>
-          </div>
-        </body>
-        </html>`;
-}
+                    </body>
+                  </html>`;
+    return html;
+  } catch (error) {
+    const html = `
+            <!DOCTYPE html>
+            <html>
+            <head>
+              <title>Wizard News</title>
+              <link rel="stylesheet" href="/style.css" />
+            </head>
+            <body>
+              <header><img src="/logo.png"/>Wizard News</header>
+              <div class="not-found">
+                <p>404: Page Not Found</p>
+              </div>
+            </body>
+            </html>`;
+    return html;
+  }
+};
+
 module.exports = { postDetails: postDetails };
