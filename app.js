6a4557e41c271700b4fe96afbe428119225c19e5
MonkComputer <bmonk1996@yahoo.com>
2023-02-27 13:21:39 -0600

    links set up

--- app.js
+++ app.js
@@ -5,22 +5,75 @@ const postBank = require('./postBank')
 
 
 app.use(morgan('dev'));
+app.use(express.static('public'))
+
 
 app.get("/", (req, res) => {
   const posts = postBank.list();
-
+console.log('hello')
   const html = `<!DOCTYPE html>
   <html>
   <head>
     <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
   </head>
   <body>
-    <ul>
-      ${posts.map(post => `<li>${post.title}</li>`)}
-    </ul>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+      ${posts.map(post => `
+        <div class='news-item'>
+          <p>
+            <span class="news-position">${post.id}. ▲</span>
+            <a href="/posts/${post.id}">${post.title}</a>
+            <small>(by ${post.name})</small>
+          </p>
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${post.date}
+          </small>
+
+        </div>`
+      ).join('')}
+    </div>
   </body>
-  </html>`
+</html>`
+  res.send(html)
 })
+
+app.get("/posts/:id",(req,res)=>{
+
+  index = req.params.id
+  const post = postBank.find(index)
+  const html = `<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+        <div class='news-item'>
+          <p>
+            <span class="news-position">${post.id}. ▲</span>
+            ${post.title}
+            <small>(by ${post.name})</small>
+          </p>
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${post.date}
+          </small>
+          ${post.content}
+        </div>
+    </div>
+  </body>
+</html>`
+  
+  
+  
+
+  res.send(html)
+
+})
+
 
 const PORT = 1337;
 
